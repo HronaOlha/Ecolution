cea24e2f911daf4d81e7d376f7afb8fcb3885b80
Грона Оля <grona.hell@gmail.com>
2024-01-19 21:04:48 +0200

    change h1 in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
       </div>
-      <h1>!TEST TEST TEST!</h1>
+      <h1>!NEW NEW NEW!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
