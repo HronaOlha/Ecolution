e40fdfe74693b9fccab1c5cb69ef17431c05e43d
Грона Оля <grona.hell@gmail.com>
2024-01-21 21:39:19 +0200

    fix/Logo-element add new svg files

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,7 @@
 // import Emblem from "../../images/emblem.svg";
 // import Ecosolution from "../../images/ecosolution.svg";
+import Emblem from "../../images/emb.svg";
+import Ecosolution from "../../images/eco.svg";
 import Slogan from "../../images/slogan.svg";
 
 import { LogoContainer, Svg } from "./Logo.styled";
@@ -15,13 +17,13 @@ const Logo = () => {
           <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
         </Svg>
       </div> */}
-      <Svg width="63px" height="21px">
-        <title>Greenergy for life</title>
-        <use href={Slogan + "#slogan"} width="100%" height="100%" />
+      <Svg width="31px" height="18px">
+        {/* <title>Greenergy for life</title> */}
+        <use href={Emblem + "#emblem"} width="100%" height="100%" />
       </Svg>
-      <Svg width="63px" height="21px">
-        <title>Greenergy for life</title>
-        <use href={Slogan + "#slogan"} width="100%" height="100%" />
+      <Svg width="170px" height="30px">
+        {/* <title>Greenergy for life</title> */}
+        <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
       </Svg>
       <Svg width="63px" height="21px">
         <title>Greenergy for life</title>
