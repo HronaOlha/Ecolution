b36cce5a60f243512a2daab194fd5ee05ff8b2b9
Грона Оля <grona.hell@gmail.com>
2024-01-21 22:06:57 +0200

    fix/Logo-element add dataURItoBlobUrl function

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,6 +5,16 @@ import Ecosolution from "../../images/eco.svg";
 import Slogan from "../../images/slogan.svg";
 
 import { LogoContainer, Svg } from "./Logo.styled";
+
+const dataURItoBlobUrl = (dataURI) => {
+  const svg = decodeURI(dataURI).split(",")[1];
+  const blob = new Blob([svg], { type: "image/svg+xml" });
+
+  return URL.createObjectURL(blob);
+};
+
+const blobUrl = dataURItoBlobUrl(Ecosolution);
+
 const Logo = () => {
   return (
     <LogoContainer href="#main" aria-label="logo">
@@ -17,11 +27,13 @@ const Logo = () => {
           <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
         </Svg>
       </div> */}
-      <Svg>
+      <Svg width="31px" height="18px">
+        {/* <title>Greenergy for life</title> */}
         <use href={Emblem + "#emblem"} width="100%" height="100%" />
       </Svg>
-      <Svg>
-        <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
+      <Svg width="170px" height="30px">
+        {/* <title>Greenergy for life</title> */}
+        <use href={`${blobUrl}#ecosolution`} width="100%" height="100%" />
       </Svg>
       <Svg width="63px" height="21px">
         <title>Greenergy for life</title>
