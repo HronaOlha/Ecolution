40570268be80550719c887ae194bdde44f87034b
Грона Оля <grona.hell@gmail.com>
2024-02-02 19:23:28 +0200

    fix/Logo-element add link in use atribute

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,8 @@
 // import Emblem from "../../images/emblem.svg";
 // import Ecosolution from "../../images/ecosolution.svg";
-import Emblem from "../../images/emb.svg";
-import Ecosolution from "../../images/eco.svg";
+
+// import Emblem from "../../images/emb.svg";
+// import Ecosolution from "../../images/eco.svg";
 import Slogan from "../../images/slogan.svg";
 
 import { LogoContainer, Svg } from "./Logo.styled";
@@ -18,13 +19,22 @@ const Logo = () => {
           <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
         </Svg>
       </div> */}
-      <Svg width="31px" height="18px">
+
+      {/* <Svg width="31px" height="18px">
         <title>Greenergy for life</title>
         <use href={Emblem + "#emblem"} width="100%" height="100%" />
       </Svg>
       <Svg width="170px" height="30px">
         <title>Greenergy for life</title>
         <use href={Ecosolution + "#ecosolution"} width="100%" height="100%" />
+      </Svg> */}
+      <Svg width="63px" height="21px">
+        <title>Greenergy for life</title>
+        <use
+          href="https://raw.githubusercontent.com/HronaOlha/Ecosolution/main/src/images/eco.svg"
+          width="100%"
+          height="100%"
+        />
       </Svg>
       <Svg width="63px" height="21px">
         <title>Greenergy for life</title>
